0630c4a1143a6a14c07f6f63c8bb7e59e42698ef
mahirsiam2004 <mahirsiam2004@gmail.com>
2025-11-27 14:16:54 +0600

    fixed view details bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
+// frontend/next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
